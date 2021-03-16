78e063b5d431bea971170a453706c46cf4901f02
dnotrad <64872689+dnotrad@users.noreply.github.com>
2021-03-16 21:53:47 +0300

    add root

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from "react";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      {/* мой хедер пусть тут полежит пока */}
+      <Header />
       First money, Andrey! Congratulate you
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import s from "./Header.module.css"
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className={s.header}>
+            Шапка
         </div>
     );
 };
